50be13ccbb099bcef5273777223923c68cf8efda
jplasencia <jplasencia@ES-27T7S93.getronics.int-it.net>
2021-10-05 19:10:14 +0200

    subida

---
+++ src/main/java/com/master/java/cero/experto/MasterCompletoJavaCeroAExpertoApplication.java
@@ -0,0 +1,16 @@
+package com.master.java.cero.experto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterCompletoJavaCeroAExpertoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterCompletoJavaCeroAExpertoApplication.class, args);
+		
+		String nombre = "jose";
+		System.out.println(nombre);
+	}
+
+}
---
+++ src/test/java/com/master/java/cero/experto/MasterCompletoJavaCeroAExpertoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.master.java.cero.experto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterCompletoJavaCeroAExpertoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
