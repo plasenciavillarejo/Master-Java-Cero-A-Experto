448cc3d1eb14844ab7b834426d9cca4c214054e1
Jose Plasencia Villarejo <plasenciavillarejo@gmail.com>
2022-06-29 15:45:17 +0200

    Clase 261: Método Join vs sleep - OKEY

--- src/main/java8/com/master/java8/cero/experto/hilos/ejemplos/EjemploInterfaceRunnableJava8.java
+++ src/main/java8/com/master/java8/cero/experto/hilos/ejemplos/EjemploInterfaceRunnableJava8.java
@@ -35,8 +35,15 @@ public class EjemploInterfaceRunnableJava8 {
 	}
 
 	*/
+		
+		
 	// Ejecutando el proceso con expresiones lambda.
 	
+	// Procedemos a valir el estado en el que se encuentra nuestro main.
+	
+	Thread principal = Thread.currentThread();
+		
+		
 	Runnable viaje = () -> {
 			for(int i=0; i<10; i++) {
 				System.out.println(i + " - " + Thread.currentThread().getName());
@@ -52,11 +59,44 @@ public class EjemploInterfaceRunnableJava8 {
 		};
 	
 	
-	new Thread(viaje,"Canada").start();
-	new Thread(viaje,"Italia").start();
-	new Thread(viaje,"Francia").start();
-	new Thread(viaje,"Rusia").start();
-	new Thread(viaje, "Alemania").start();
+	Thread v1 = new Thread(viaje,"Canada");
+	Thread v2 = new Thread(viaje,"Italia");
+	Thread v3 = new Thread(viaje,"Francia");
+	Thread v4 = new Thread(viaje,"Rusia");
+	Thread v5 = new Thread(viaje, "Alemania");
+	
+	v1.start();
+	v2.start();
+	v3.start();
+	v4.start();
+	v5.start();
+	
+	// Vamos a unir el Thread principal con el Thread de viaje 1, de está forma esperamos a que finalicen
+	// todos los Threads para continuar con la ejecución.
+	try {
+		v1.join();
+		v2.join();
+		v3.join();
+		v4.join();
+		v5.join();
+		System.out.println(principal.getState());
+	} catch (InterruptedException e) {
+		System.out.println("Se ha producido un error en la union de los join" + e.getMessage());
+	}
+	
+	try {
+		Thread.sleep(10000);
+	} catch (InterruptedException e) {
+		e.printStackTrace();
+	}
+	
+	
+	System.out.println("\n==========================================================================================");
+	System.out.println("==========================================================================================");
+	System.out.println(" === Cuando finalizan todos los Threads de viajes se ejecuta nuestro último mensaje === \n");
+	
+	System.out.println("Continuando con la ejecución del método main");
+	
 }
 	
 	
