c2c1bc22fb190ce0d4e24f4a63a0537fc0ce28fc
Jose Plasencia Villarejo <plasenciavillarejo@gmail.com>
2022-07-01 00:35:58 +0200

    Clase 267: Tareas programadas que se repiten cada cierto tiempo- OKEY

--- src/main/java8/com/master/java8/cero/experto/hilos/ejemplosTimer/EjemploTareaTimerPeriodica.java
+++ src/main/java8/com/master/java8/cero/experto/hilos/ejemplosTimer/EjemploTareaTimerPeriodica.java
@@ -3,7 +3,9 @@ package com.master.java8.cero.experto.hilos.ejemplosTimer;
 
 import java.awt.Toolkit;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.IllformedLocaleException;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -21,6 +23,7 @@ public class EjemploTareaTimerPeriodica {
 		// Se indica el beep de windows.
 		//Toolkit toolkit = new Toolkit.getDefaultToolkit();
 		
+		
 		timer.schedule(new TimerTask() {
 			// Variable local para poder pasarle el contador, se sustituye por AtomicInteger
 			//int contador = 3;
@@ -32,8 +35,12 @@ public class EjemploTareaTimerPeriodica {
 			@Override
 			public void run() {
 				
+				String[] diaSemana = validacionDia().split("-");
+				
+				Integer diaNumero = Integer.valueOf(diaSemana[1]);
+				
 				int decrementarContador = contadorAtomic.getAndDecrement();
-				if (decrementarContador > 0) {
+				if (decrementarContador > 0 && diaNumero == 6) {
 					
 					//toolkit.beep();
 					System.out.println("Tarea " + decrementarContador + " periódica ejecutada el día: " + fechaFormateada + ", nombre del Hilo (Thread): "
@@ -53,8 +60,44 @@ public class EjemploTareaTimerPeriodica {
 			 (TiempoRetardo, TiempoRepeticion) -> (0,1) Se ejcuta directamente.
 			 */
 		//}, 0, 1);
-		}, 0, 10000);	
+		// 1 hora = 3600.
+		}, 0, 86400);	
 		System.out.println("Retrasamos la tarea inicial en 5 segundos ...");
 	}
 
+	// Función que nos indica el día de la semana en la que nos encontramos y la fecha de ejecución.
+	public static String validacionDia() {
+		Calendar calendario = Calendar.getInstance();
+		// Nos indica el día de la semana que nos encontramos.
+		
+		Date fecha = calendario.getTime();
+		
+		// Formateo de fecha
+		SimpleDateFormat formatoFecha = new SimpleDateFormat("dd/MM/yyyy");
+		
+		String fechaSemana = formatoFecha.format(fecha);
+		
+		/* Para sacar el día de la semana lo obtenemos mediante Calendar.DAY_OF_WEEK.
+			0 - DOMINGO
+			7 - SABADO
+		*/
+		int diaSemana = calendario.get(Calendar.DAY_OF_WEEK);
+		
+		
+		System.out.println(fechaSemana + " - " + diaSemana);
+		return fechaSemana + "-" + diaSemana;
+		
+	}
+	
+	
+	public static String validacionHora() {
+		
+		Date date = new Date();
+		SimpleDateFormat formato = new SimpleDateFormat("HH:mm:ss");
+		
+		String hora = formato.format(date);
+		
+		return hora;
+	}
+	
 }
