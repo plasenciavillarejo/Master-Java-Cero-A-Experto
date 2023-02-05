05ba8e23eec56818bc70c9acdec6895729977592
Jose Plasencia Villarejo <plasenciavillarejo@gmail.com>
2023-02-05 13:15:07 +0100

    Clase 319: Patron Singleton

---
+++ src/main/java15/PatronesDiseño/com/master/java15/conexion/ConexionBdSingleton.java
@@ -0,0 +1,5 @@
+package com.master.java15.conexion;
+
+public class ConexionBdSingleton {
+
+}
---
+++ src/main/java15/PatronesDiseño/com/master/java15/patrones/EjemploSingleton.java
@@ -0,0 +1,10 @@
+package com.master.java15.patrones;
+
+public class EjemploSingleton {
+
+	// Los patrones singleton solo creara una sola instancia de el objeto.
+	
+	public static void main(String[] args) {
+
+	}
+}
